c60d787033b2ea2fc4d727a444bba0808c0ae979
Your Name <you@your-domain.com>
2020-08-12 02:11:33 +0900

    second

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 console.log(123);
+
+const express = require('express');
+const app = express();
+
+app.use('/', (req, res) => {
+  console.log('hello node server');
+})
+app.listen(3000, () => {
+  console.log('연결중')
+})
